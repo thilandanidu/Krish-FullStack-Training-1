4a082a8777ebcc80958fd7bcbaafdf270b6326e4
Dilhan9 <45040107+Dilhan9@users.noreply.github.com>
2022-08-21 19:29:12 +0530

    Delete Fuel Distribution System/Backend directory

--- Fuel Distribution System/Backend/Dispactch server/fuel-application-backend/src/main/java/com/fuel/fuelapplication/repository/DispatchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fuel.fuelapplication.repository;
-
-import com.fuel.fuelapplication.model.Dispatch;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DispatchRepository extends JpaRepository<Dispatch, Long> {
-}
--- Fuel Distribution System/Backend/Order-service/fual-distribution-order/src/main/java/com/project/fual/fualdistributionorder/service/orderService.java
+++
@@ -1,8 +0,0 @@
-package com.project.fual.fualdistributionorder.service;
-
-import com.project.fual.fualdistributionorder.model.Order;
-import org.springframework.stereotype.Service;
-
-public interface orderService {
-    public Order save(Order order);
-}
